e12123c87e5c3ec4a4a4a91dbbb8ccd7e3fe4f49
watanabeyu <lovegeunyoung@sina.com>
2017-10-25 14:59:21 +0800

    update

--- src/views/mobile/index.js
+++ src/views/mobile/index.js
@@ -1,7 +1,3 @@
-import '^/css/common.scss'//引用样式示例
-
 document.getElementById('app').innerHTML = (`
     <h1><i class="fa fa-diamond"></i> 太棒啦，这个是手机端！</h1>
-    <p>现在让我开始用webpack脚手架开发我们的页面吧！</p>
-    <p class="muted">Let's start our simple-page project with webpack-cli</p>
 `);
